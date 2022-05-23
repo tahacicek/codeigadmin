25547dde163bf120d41a5675456d3c41f9581135
Taha Çiçek <ttahacicek@gmail.com>
2022-05-24 01:54:27 +0300

    Admin Template İnser ve Update Yapıldı.

---
+++ application/controllers/Members.php
@@ -0,0 +1,88 @@
+<?php
+
+class Members extends CI_Controller
+{
+
+    public function __construct()
+    {
+
+        parent::__construct();
+        $this->viewFolder = "members_v";
+        $this->load->model("member_model");
+    }
+
+
+    public function index()
+    {
+        /* Tablodan verilerin getirilmesi */
+        $items = $this->member_model->get_all();
+        /* Verinin incelenmesi */
+        //print_r($items);
+
+        /* View yapısının kurulması */
+
+        $viewData = new stdClass();
+
+        $viewData->viewFolder = $this->viewFolder;
+        $viewData->subviewFolder = "list";
+        $viewData->items = $items;
+
+        $this->load->view("{$viewData->viewFolder}/{$viewData->subviewFolder}/index", $viewData);
+    }
+
+    public function newForm()
+    {
+
+        /* View yapısının kurulması */
+        $viewData = new stdClass();
+        $viewData->viewFolder = $this->viewFolder;
+        $viewData->subviewFolder = "add";
+
+        $this->load->view("{$viewData->viewFolder}/{$viewData->subviewFolder}/index", $viewData);
+    }
+
+    public function insert()
+    {
+
+
+        $data = array(
+
+            "email"     => $this->input->post("email")
+
+        );
+
+        $insert = $this->member_model->insert($data);
+
+
+        if ($data) {
+            echo "Başarılı";
+        } else {
+            echo "Başarısız";
+        }
+    }
+
+
+    public function delete($id)
+    {
+        $delete = $this->member_model->delete(array(
+            "id" => $id
+        ));
+        if ($delete) {
+
+            echo "Silme Başarılı";
+        } else {
+            echo "Silinemedi";
+        }
+    }
+
+    public function update($id)
+    {
+        $data = array(
+            'id' => $id,
+           
+        );
+
+        $this->db->where('id', $id);
+        $this->db->update($data);
+    }
+}
